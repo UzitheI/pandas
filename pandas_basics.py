9b096217b932df82f2b10f90743aeb60d1fd7db4
UzitheI <ujjwalpuri221@gmail.com>
2024-07-21 05:38:18 +0545

    intro to pandas

---
+++ pandas_basics.py
@@ -0,0 +1,16 @@
+#note
+# Pandas was built on top of numpy, all data are not array which makes pandas an essential library to data process
+
+# index axis=0 is a row , index axis=1 are columns
+
+# a data set is given to us, now we process these data, the first thing is to look into the table like in a relational database and then differentiate types of their columns
+
+#another thing we can do is perform statistical operations on the data
+
+# median, 50th percentile
+#mean
+#variance, how much variance means how much spread is in the data
+
+#Skewness
+
+#we can perform various statistical operations with pandas
